3af75f7ec9f23598456f342039e70f602d66ccbf
Rosemaria Rosier <rosierosier@gmail.com>
2016-02-17 13:33:43 -0500

    updated refactor.js to include answer for number 2

--- refactor.js
+++ refactor.js
@@ -51,6 +51,9 @@ setTimeout(sayHi, 2000);
 
 // Put your answer below -------------------------
 
+setTimeout(function() {
+  alert("Hello, World!");
+}, 2000);
 
 // -----------------------------------------------
 
