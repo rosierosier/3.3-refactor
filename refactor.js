54b5d9f2436cab2d6f0d69824d1e2c7ea848c456
Rosemaria Rosier <rosierosier@gmail.com>
2016-02-17 13:54:27 -0500

    updated refactor.js to include answer for number 5

--- refactor.js
+++ refactor.js
@@ -145,6 +145,15 @@ var spanishColor = function(colorName) {
 
 // Put your answer below -------------------------
 
+var spanishColor = function(colorName) {
+  colorName = {
+    "rojo":"#ff0000",
+    "blanco":"#ffffff",
+    "azul":"#0000ff",
+    "verde":"#00ff00",
+    "negro":"#000000"
+  }
+};
 
 // -----------------------------------------------
 
