10ec06a52368921e66bbe0e352aaac1d546cf700
Rosemaria Rosier <rosierosier@gmail.com>
2016-02-17 13:38:43 -0500

    updated refactor.js to include answer for number 4

--- refactor.js
+++ refactor.js
@@ -107,6 +107,10 @@ var reverseStr = function(str) {
 
 // Put your answer below -------------------------
 
+var reverseStr = function(str) {
+  return: str.split('').reverse().join('');
+}
+
 
 // -----------------------------------------------
 
